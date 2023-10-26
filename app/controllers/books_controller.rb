c1842526ccdcbd41ee50d9df0602841be68c7d9c
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-10-26 10:59:34 +0000

    [Add]初テスト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,18 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(id: current_user)
   end
 
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
-  def
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
